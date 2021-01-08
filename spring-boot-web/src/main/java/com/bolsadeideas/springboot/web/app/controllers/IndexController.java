bbad9bdce61e00a2025224f4dd55a0fd19363fe0
Cristian Arevalo <cristian.arevalo@norsoft.com.co>
2021-01-07 22:36:32 -0500

    Segundo modulo del curso(Thymeleaf,Anotaciones: PathVariable, RequestParam, entre otras.)

--- spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,15 +1,61 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
+	
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
 
 	@GetMapping({ "/index", "/", "/home" })
 	public String index(Model model) {
-		model.addAttribute("titulo", "Hola, Sirve de forma buena");
+		model.addAttribute("titulo", textoIndex);
 		return "index";
+	}
+
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Camilo");
+		usuario.setApellido("Arevalo");
+		usuario.setEmail("cristian@gmail.com");
+		model.addAttribute("usuario", usuario);
+		model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
+		return "perfil";
+	}
+
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+
+		model.addAttribute("titulo", textoListar);
+		return "listar";
+	}
+
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios() {
+		List<Usuario> usuarios = new ArrayList<>();
+		usuarios.add(new Usuario("Cristian", "Arevalo", "cristian@gmail.cmom"));
+		usuarios.add(new Usuario("Camilo", "Gutierrez", "camilo@gmail.cmom"));
+		usuarios.add(new Usuario("Antonia", "Rua", "Anto@gmail.cmom"));
+
+		return usuarios;
 	}
 }
