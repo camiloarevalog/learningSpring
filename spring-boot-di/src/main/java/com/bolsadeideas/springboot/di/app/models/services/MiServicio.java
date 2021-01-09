7bb10a4a9e07c0e8d588101101eed161b8f03649
Cristian Arevalo <cristian.arevalo@norsoft.com.co>
2021-01-08 22:03:12 -0500

    Proyecto inyectando dependencias

---
+++ spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/services/MiServicio.java
@@ -0,0 +1,14 @@
+package com.bolsadeideas.springboot.di.app.models.services;
+
+import org.springframework.stereotype.Component;
+
+@Component("miServicioSimple")
+public class MiServicio implements IServicio {
+
+	@Override
+	public String operacion() {
+		return "ejecutando un proceso";
+	}
+	
+
+}
